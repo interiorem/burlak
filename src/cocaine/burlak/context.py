86b93d6ccf9e3e0af888096422c98922817fb78d
karitra <karev.alexandre@gmail.com>
2017-09-26 21:12:34 +0300

    refact: transform context to named tuple

--- src/cocaine/burlak/context.py
+++ src/cocaine/burlak/context.py
@@ -7,19 +7,13 @@ LoggerSetup = namedtuple('LoggerSetup', [
 ])
 
 
-class Context(object):
-    '''Application wide internal state holders, handlers, e.g.:
-    - config
-    - logger
-    '''
-    def __init__(self, logger_setup, config):
-        self._logger_setup = logger_setup
-        self._config = config
-
-    @property
-    def logger_setup(self):
-        return self._logger_setup
-
-    @property
-    def config(self):
-        return self._config
+'''Application wide internal state holders, handlers, e.g.:
+- config
+- logger
+- sentry_wrapper
+'''
+Context = namedtuple('Context', [
+    'logger_setup',
+    'config',
+    'sentry_wrapper'
+])
