03eecf6e69a7908939d2ecdfe73fe5c89f8e2de0
karitra <karev.alexandre@gmail.com>
2018-02-22 13:19:24 +0300

    refact: update of filter_control utility

--- src/cocaine/burlak/filter_control/__main__.py
+++ src/cocaine/burlak/filter_control/__main__.py
@@ -10,6 +10,13 @@ from ..config import Config
 DEFAULT_FILTER_PATH = '/darkvoice/control_filter'
 
 
+def get_secure_unicorn():
+    config = Config(SharedStatus())
+    config.update()
+
+    return SecureServiceFabric.make_secure_adaptor(
+        Service('unicorn'), *config.secure, endpoints=config.locator_endpoints)
+
 @gen.coroutine
 def update(unicorn, path, record):
     print 'writing to path {}, record {}'.format(path, record)
@@ -34,7 +41,7 @@ def get(unicorn, path):
     if version == -1:
         print 'no record'
 
-    print 'data is\n{}'.format(data)
+    raise gen.Return(data)
 
 @gen.coroutine
 def remove_node(unicorn, path):
@@ -52,33 +59,45 @@ def remove_node(unicorn, path):
     yield ch.rx.get()
 
 
-@click.command()
+@click.group()
+def cli():
+    click.echo('processing commnads...')
+
+
+@cli.command()
 @click.option('--path', default=DEFAULT_FILTER_PATH, help='subscription node, default {}'.format(DEFAULT_FILTER_PATH))
-@click.option('--record/--no-record', default=True, help='view unicorn record (default operation)')
-@click.option('--delete/--no-delete', default=False, help='delete node and exit')
+def view(path):
+    '''View control filter record'''
+    unicorn = get_secure_unicorn()
+    click.echo('loading record...')
+    data = IOLoop.current().run_sync(lambda: get(unicorn, path))
+    print 'record: \n{}'.format(data)
+
+@cli.command()
+@click.option('--path', default=DEFAULT_FILTER_PATH, help='node to delete, default {}'.format(DEFAULT_FILTER_PATH))
+def delete(path):
+    '''Delete specified node'''
+    unicorn = get_secure_unicorn()
+    click.echo('deleting record')
+    IOLoop.current().run_sync(lambda: remove_node(unicorn, path))
+
+
+@cli.command()
+@click.option('--path', default=DEFAULT_FILTER_PATH, help='subscription node, default {}'.format(DEFAULT_FILTER_PATH))
 @click.option('--apply-control/--no-apply-control', default=False, help='apply_control flag')
 @click.option('--white-list', default='', type=click.STRING, help='comma separated prefixes for white list')
-def main(path, record, delete, apply_control, white_list):
-    config = Config(SharedStatus())
-    config.update()
+def modify(path, apply_control, white_list):
+    '''Modify or delete control filter record'''
 
-    unicorn = SecureServiceFabric.make_secure_adaptor(
-        Service('unicorn'), *config.secure, endpoints=config.locator_endpoints)
-
+    unicorn = get_secure_unicorn()
     white_list = [item.strip() for item in white_list.split(',') if item]
 
-    if record:
-        return IOLoop.current().run_sync(lambda: get(unicorn, path))
-
-    if delete:
-        return IOLoop.current().run_sync(lambda: remove_node(unicorn, path))
-    else:
-        r = dict(
-            apply_control=bool(apply_control),
-            white_list=white_list
-        )
-        return IOLoop.current().run_sync(lambda: update(unicorn, path, r))
+    r = dict(
+        apply_control=bool(apply_control),
+        white_list=white_list
+    )
+    return IOLoop.current().run_sync(lambda: update(unicorn, path, r))
 
 
 if __name__ == '__main__':
-    main()
+    cli()
