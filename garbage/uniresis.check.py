d53a8540a635c303fdfade15c782243d41ffe947
karitra <karev.alexandre@gmail.com>
2018-04-20 17:34:44 +0300

    chore: add uniresis check

---
+++ garbage/uniresis.check.py
@@ -0,0 +1,16 @@
+from cocaine.services import Service
+
+from tornado import gen
+from tornado.ioloop import IOLoop
+
+@gen.coroutine
+def extra(service):
+    ch = yield service.extra()
+    data = yield ch.rx.get()
+
+    print 'data {}'.format(data)
+
+    raise gen.Return(data)
+
+uniresis = Service('uniresis')
+IOLoop.current().run_sync(lambda : extra(uniresis))
