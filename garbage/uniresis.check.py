0eed7c6282ca8205db7f1a2ae73d5e4ea941b617
karitra <karev.alexandre@gmail.com>
2018-04-23 13:31:42 +0300

    chore: update uniresis check

--- garbage/uniresis.check.py
+++ garbage/uniresis.check.py
@@ -4,6 +4,16 @@ from tornado import gen
 from tornado.ioloop import IOLoop
 
 @gen.coroutine
+def uuid(service):
+    ch = yield service.uuid()
+    data = yield ch.rx.get()
+
+    print 'data {}'.format(data)
+
+    raise gen.Return(data)
+
+
+@gen.coroutine
 def extra(service):
     ch = yield service.extra()
     data = yield ch.rx.get()
@@ -13,4 +23,5 @@ def extra(service):
     raise gen.Return(data)
 
 uniresis = Service('uniresis')
+IOLoop.current().run_sync(lambda : uuid(uniresis))
 IOLoop.current().run_sync(lambda : extra(uniresis))
